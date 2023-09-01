7d553ce764c577471f261052476536ad1e5fc0e7
Kett Lovahr <catusfelony@gmail.com>
2023-09-01 18:56:30 -0300

    Draw the rest of the owl

---
+++ gol.glsl
@@ -0,0 +1,38 @@
+#version 330
+in vec2 fragTexCoord;
+in vec4 fragColor;
+
+out vec4 finalColor;
+
+uniform sampler2D texture0;
+uniform vec2 resolution;
+
+float sample_position(vec2 pos) {
+    pos = vec2(pos.x, -pos.y);
+    return texture(texture0, pos).r;
+}
+
+void main() {
+
+    vec2 pixelsize = vec2(1.0, 1.0) / resolution;
+
+    float alive = sample_position(fragTexCoord);
+
+    int count = 0;
+    for (int i = -1; i < 2; i++) {
+        for (int j = -1; j < 2; j++) {
+            if (abs(i) + abs(j) != 0) {
+                if (sample_position(fragTexCoord + (vec2(i, -j) * pixelsize)) == 1.0) {
+                    count++;
+                }
+            }
+        }
+    }
+    finalColor = vec4(vec3(0.0), 1.0);
+    if (alive == 1.0) {
+        if (count == 2) {finalColor = vec4(vec3(1.0), 1.0);}
+        if (count == 3) {finalColor = vec4(vec3(1.0), 1.0);}
+    } else {
+        if (count == 3) {finalColor = vec4(vec3(1.0), 1.0);}
+    }
+}
--- main.c
+++ main.c
@@ -1,34 +1,53 @@
 #include <stdlib.h>
 #include <raylib.h>
 
-#define WIDTH 1024
-#define HEIGHT 768
+#define WIDTH 1600
+#define HEIGHT 900
 
 int main() {
     InitWindow(WIDTH, HEIGHT, "GPU Game Of Life");
-    SetTargetFPS(60);
-
-    Image image = LoadImage("assets/gol.png");
+    SetTargetFPS(30);
 
     RenderTexture2D frames[2];
     frames[0] = LoadRenderTexture(WIDTH, HEIGHT);
+    SetTextureWrap(frames[0].texture, TEXTURE_WRAP_REPEAT);
+    SetTextureFilter(frames[0].texture, TEXTURE_FILTER_POINT);
     frames[1] = LoadRenderTexture(WIDTH, HEIGHT);
+    SetTextureWrap(frames[1].texture, TEXTURE_WRAP_REPEAT);
+    SetTextureFilter(frames[1].texture, TEXTURE_FILTER_POINT);
+    int current = 0;
+
+    Shader gol_shader = LoadShader(NULL, "gol.glsl");
+    Vector2 resolution = {WIDTH, HEIGHT};
+    int resolution_loc = GetShaderLocation(gol_shader, "resolution");
+    SetShaderValue(gol_shader, resolution_loc, &resolution, SHADER_UNIFORM_VEC2);
 
     BeginTextureMode(frames[0]);
         ClearBackground(BLACK);
         for (int i = 0; i < WIDTH; i++){
             for (int j = 0; j < HEIGHT; j++) {
-                float color = (float)rand() / (float)RAND_MAX;
+                float color = (float)(rand() % 2);
                 DrawPixel(i, j, ColorFromNormalized((Vector4){color, color, color, 1.0}));
             }
         }
     EndTextureMode();
 
     while (!WindowShouldClose()) {
+        BeginTextureMode(frames[1 - current]);
+        ClearBackground(BLACK);
+        BeginShaderMode(gol_shader);
+        DrawTexture(frames[current].texture, 0, 0, WHITE);
+        EndShaderMode();
+        EndTextureMode();
+
+        current = 1 - current;
+
         BeginDrawing();
-        DrawTexture(frames[0].texture, 0, 0, WHITE);
+        ClearBackground(BLACK);
+        DrawTexture(frames[current].texture, 0, 0, WHITE);
         EndDrawing();
-        
+
+
     }
 
     CloseWindow();
